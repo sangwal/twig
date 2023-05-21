f947b79dce6c524890d7afff41e22cdafa6841a3
Sunil Sangwal <sunil.sangwal@gmail.com>
2023-05-21 14:18:48 +0530

    initial import

---
+++ twig.py
@@ -0,0 +1,542 @@
+#! python3
+"""
+    twig.py -- TeacherWIse [timetable] Generator
+    
+    This file is up-to-date version of timetable-teacherwise-gen.py
+
+    A python script to generate Teacherwise timetable from Classwise timetable (and vice versa -- yet to be implemented).
+    
+    The classwise timetable is read from CLASSWISE sheet and the generated teacherwise
+    timetable is saved in TEACHERWISE sheet of the same input workbook.
+
+    Written by Sunil Sangwal (sunil.sangwal@gmail.com)
+    Date written: 20-Apr-2022
+    Last Modified: 18-May-2023
+"""
+import argparse
+import re
+import time
+import sys
+
+import openpyxl
+from openpyxl.styles import Alignment
+from openpyxl.utils import get_column_letter
+
+# configuration variables before running the script
+
+expand_names = False    # set this to True to write full names of teachers
+
+# filename = 'C:\\Users\\acer\\Downloads\\CLASSWISE TIMETABLE 2022-23.xlsx'
+# filename = 'C:\\Users\\acer\\Documents\\classwise-timetable.xlsx'
+# output_filename = 'C:\\Users\\acer\\Documents\\TEACHERWISE TIMETABLE-tmp2.xlsx'
+
+def escape_special_chars(c):
+    if c == '\n':
+        c = '\\n'
+    elif c == '\t':
+        c = '\\t'
+    return c
+
+def expand_days(days):
+    """
+        Parameter
+            days : eg. "1-2, 3, 4-6"
+        
+        Returns:
+            [1, 2, 3, 4, 5, 6]
+    """
+    ret = []
+    if days.find(',') >= 0:
+        groups = days.split(',')
+    else:
+        groups = [days]
+
+    for days in groups:
+        if days.find('-') >= 0:
+            start_day, end_day = days.split('-')
+            start_day = int(start_day)
+            end_day = int(end_day)
+
+            # swap if in reverse order: 6-4 is same as 4-6
+            if end_day < start_day:
+                start_day, end_day = end_day, start_day
+
+            for i in range(start_day, end_day+1):
+                ret.append(i)
+        else:
+            ret.append(int(days))
+    return ret
+
+def compress_days(days): # **TODO:**
+    """
+        Parameter:
+            days -- a list containing days in expanded form eg [1,2,3,5,6]
+        
+        Returns:
+            a string of the form "1-3, 5-6"
+    """
+    days = sorted(days)
+    ret = []
+    start = end = 0
+    for i in range(len(days) - 1):
+        if days[i + 1] - days[i] == 1:
+            continue
+        else:
+            end = i
+            if start == end:
+                s = f"{days[start]}"
+            else:
+                s = f"{days[start]}-{days[end]}"
+            ret.append(s)
+            start = i + 1
+
+    end = i+1
+    if start == end:
+        ret.append(f"{days[start]}")
+    else:
+        ret.append(f"{days[start]}-{days[end]}")
+
+    retval =  ", ".join(ret)
+    return retval
+
+# print(compress_days([1, 2, 3, 4, 5, 6]))
+# exit(0)
+
+def count_days(days):
+    return len(set(expand_days(days)))
+
+def count_periods(teacher, timetable):
+    period_count = {}
+    for period_info in timetable[teacher]:
+        column, class_name, days, subject = period_info
+        # print(period_info)
+        days = expand_days(days)
+        if column not in period_count:
+            period_count[column] = []
+
+        period_count[column].extend(days)   # combine two lists
+
+    total_periods = 0
+    
+    for column in period_count:
+        periods = len(set(period_count[column]))   # remove duplicates
+        total_periods += periods
+
+    return total_periods
+
+# print(expand_days("1-2, 3, 4-6"))
+# exit(0)
+
+def load_teacher_names(workbook):
+    # the sheet "TEACHERS" contains data about teacher in format
+    # TEACHER-CODE ; FULLNAME
+    sheet = workbook['TEACHERS']
+    
+    teacher_names = {}
+    
+    row = 2
+    while True:
+        teacher_code = sheet.cell(row, 1).value
+        if teacher_code == None:
+            break
+
+        if teacher_code in teacher_names:
+            # teacher code has been repeated
+            raise Exception(f"Teacher code '{teacher_code}' has been used more than once. Modify TEACHERS sheet to remove the error.")
+
+        teacher_names[teacher_code] = sheet.cell(row, 2).value # full name
+        row += 1
+
+    # assert len(teacher_names)==27
+
+    return teacher_names
+
+def get_class_number(_class):
+    return _class[:len(_class) - 1] # remove section (for example, 'A' from '10A')
+
+def highlight_clashes(sheet, context):
+    """
+        reads teacherwise timetable and highlights possible clashes
+        by prepending **CLASH** to the offending cell
+    """
+    SEPARATOR = context['SEPARATOR']
+    CLASH_MARK = '**CLASH** '
+    total_clashes = 0
+
+    # format of line is "CLASS (1-3,5-6) SUBJECT", e.g., 10A (1-2, 4) MATH
+    p = re.compile(r'^(?P<class_name>[\w]+)\s*\((?P<days>.*)\)\s*(?P<subject>[\w -]+)$')
+    
+    row = 2
+    while True:
+        if not sheet.cell(row=row, column=1).value:
+            break
+
+        for column in range(2, 10):
+            content = sheet.cell(row, column).value
+            # skip empty cells in class timetable with a warning
+            if not content:
+                # cells in teacherwise timetable could be empty; just skip them
+                # warnings += 1
+                # print(f"Warning: Cell {get_column_letter(column)}{row} of teacherwise timetable is empty.")
+                continue
+            # content = content.replace('\n', ';')
+            # lines = content.split(";")
+            lines = content.split(SEPARATOR) # SEPARATOR is "\n" or ;
+            
+         
+            entry = {}
+
+            for line in lines:
+                line = line.strip()
+                if line == "":
+                    # skip empty lines
+                    continue
+
+                m = p.match(line)
+                if not m:
+                    print(f"\nWarning: (row={row}, column={column}) (Cell {get_column_letter(column)}{row}) in 'Teacherwise' timetable has formatting issue")
+                    print("    >>> ", line)
+                    # warnings += 1
+                    continue
+                class_name, days, subject = m.groups()
+                subject = subject.strip()
+                days = expand_days(days)
+                """
+                    Ex 1:
+                        10A (1-2) MATH
+                        10B (2-3) MATH
+                    
+                    is not a clash; but
+
+                    Ex 2:
+                        10A (1-2) MATH
+                        9B (2-3) MATH
+
+                    is a clash.
+
+                    2: [10, 9]
+                    In Example 2 above, 2nd period: classes 10 and 9 simultaneously is a clash
+                """
+
+                for day in days:
+                    if not day in entry:
+                        entry[day] = []
+                    entry[day].append(get_class_number(class_name))    # Eg., 10 (from 10A)
+
+            # after all lines in a cell have been processed
+            clash_days = []
+            for day in entry:
+                entry[day] = set(entry[day])    # remove duplicates
+                if len(entry[day]) > 1:
+                    # possible clash
+                    clash_days.append(day)
+
+            # if there are clashes, write them
+            if len(clash_days) > 0:
+                total_clashes += len(clash_days)
+                clash_days = repr(clash_days)
+                sheet.cell(row=row, column=column).value = CLASH_MARK + f"{clash_days}:\n" + sheet.cell(row=row, column=column).value
+
+        row += 1
+
+    return total_clashes
+
+def clear_sheet(sheet):
+    # clear the sheet before starting writing...
+    row = 2
+    while True:
+        if not sheet.cell(row=row, column=1).value:
+            # we have reacher EOF
+            break
+
+        for column in range(2, 11):
+            sheet.cell(row=row, column=column).value = ""
+
+        row += 1
+
+    return
+
+def generate_teacherwise(workbook):
+    if "CLASSWISE" in workbook:
+        print("Reading 'CLASSWISE' sheet... ", end='')
+        input_sheet = workbook["CLASSWISE"]
+        print("done.")
+    else:
+        print("Sheet 'CLASSWISE' not foud. Reading active sheet instead... ")
+        input_sheet = workbook.active
+
+    # if names are to be replaced with full names for teachers,
+    # then we must have 'TEACHERS' sheet in the input file
+    teacher_names = {}
+    if "TEACHERS" in book:
+        print("Reading teacher details from 'TEACHERS' sheet... ", end='')
+        teacher_names = load_teacher_names(workbook)
+        print("done.")
+
+    timetable = {}
+
+    # >>> p = re.compile(r'(?P<class>\w+)\s*\((?P<days>.*)\)\s*(?P<teacher>\w+)')
+    # >>> p.match("HIS(1-4)SD")
+    # <re.Match object; span=(0, 10), match='HIS(1-4)SD'>
+    # >>> m = p.match("HIS(1-4)SD")
+    # >>> m.groupdict()
+    # {'class': 'HIS', 'days': '1-4', 'teacher': 'SD'}
+
+    print("Processing timetable ...")
+    p = re.compile(r'^(?P<subject>[\w -.]+)\s*\((?P<days>.*)\)\s*(?P<teacher>\w+)$') # format "SUBJECT (1-3,5-6) TEACHER"
+
+    warnings = 0
+    row = 2
+    while True:
+        class_name = input_sheet.cell(row, 1).value
+        if not class_name:
+            break
+
+        periods_assigned = {}   # subjectwise keep track of how many periods have been assigned
+
+        print(f"Class: {class_name}... ", end="")
+        for column in range(2, 10):
+            content = input_sheet.cell(row, column).value
+            # skip empty cells in class timetable with a warning
+            if not content:
+                warnings += 1
+                print(f"Warning: Cell {get_column_letter(column)}{row} is empty.")
+                continue
+
+            lines = content.split(SEPARATOR) # SEPARATOR is "\n" or ;
+            for line in lines:
+                line = line.strip()
+                if line == '' or line.startswith('#'):  # ignore lines with '#' -- used as comment
+                    continue
+
+                m = p.match(line)
+                if not m:
+                    print(f"\nWarning: (row={row}, column={column}) (Cell {get_column_letter(column)}{row}) has some formatting issue")
+                    print("    >>> ", line)
+                    warnings += 1
+                    continue
+
+                subject, days, teacher = m.groups()
+                subject = subject.strip()
+                
+                if subject not in periods_assigned:
+                    periods_assigned[subject] = count_days(days)
+                else:
+                    periods_assigned[subject] += count_days(days)
+
+                if teacher not in timetable:
+                    timetable[teacher] = []
+                
+                period = column                     # column denotes "period"
+                timetable[teacher].append((period, class_name, days, subject))
+
+        # calculate the number of periods assigned to different subjects
+        periods_assigned = sorted(periods_assigned.items())
+
+        period_list = []
+        num_periods_assigned = 0
+        for subject, periods in periods_assigned:
+            num_periods_assigned += periods
+            period_list.append(f"{subject}: {periods}")
+
+        period_list.append(f"TOTAL: {num_periods_assigned}")
+        
+        # finally, write the 'SUBJECT: #periods' for all subjects to their respective classes
+        input_sheet.cell(row=row, column=10).value = ", ".join(period_list)
+
+        print("done.")
+        # process next class
+        row += 1
+        # end while loop
+
+    # source timetable update time
+    input_sheet.cell(row, 2).value = "Last updated on " + time.ctime()
+
+    # count total periods for each teacher
+    total_periods = {}
+
+    for teacher in timetable:
+        total_periods[teacher] = count_periods(teacher, timetable)
+
+    # everything has been read into the timetable
+    # now write back to a new work book
+
+    if 'TEACHERWISE' in book:
+        output_sheet = book['TEACHERWISE']
+    else:
+        print("Creating TEACHERWISE sheet... ", end='')
+        output_sheet = book.create_sheet(title='TEACHERWISE', index=1)
+        print("done.")
+
+    # change styles
+    # alignment = Alignment(horizontal='general',
+    #     vertical='top',
+    #     text_rotation=0,
+    #     wrap_text=True,
+    #     shrink_to_fit=False,
+    #     indent=0)
+
+    clear_sheet(output_sheet)
+
+    # header = ["Name", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "Periods"]
+    header = ["Name", 1, 2, 3, 4, 5, 6, 7, 8, "Periods"]
+    for column in range(2, len(header) + 1):
+        output_sheet.cell(row=1, column=column).value = header[column - 1]
+
+    unsorted_teachers = timetable.keys()
+    sorted_teachers = []
+
+    for teacher in teacher_names:
+        if teacher in unsorted_teachers:
+            sorted_teachers.append(teacher)
+
+    # check if there all teacher codes have associated full names
+    # and add teacher if his/her whose fullname is not written in the TEACHERS sheet.
+    for teacher in unsorted_teachers:   # for every teacher code in timetable ...
+        if teacher not in sorted_teachers:
+            sorted_teachers.append(teacher)
+
+    # start writing in 2nd row and then move to the following rows
+    row = 2         
+
+    for teacher in sorted_teachers:
+        periods = timetable[teacher]
+        # sheet.cell(row, 1).value = teacher # teacher code
+        if expand_names and (teacher in teacher_names):
+            output_sheet.cell(row, 1).value = teacher_names[teacher] # full teacher name
+        else:
+            output_sheet.cell(row, 1).value = teacher # abbreviation as has been used in classwise timetable
+
+        # sort day-wise
+        periods = sorted(periods, key=lambda x:x[2])
+
+        for period in periods:
+            (column, class_name, days, subject) = period
+            class_name = class_name.strip()
+            if output_sheet.cell(row, column).value:
+                output_sheet.cell(row, column).value += f"\n{class_name} ({days}) {subject}"
+            else:
+                output_sheet.cell(row, column).value = f"{class_name} ({days}) {subject}"
+
+        output_sheet.cell(row, 10).value = total_periods[teacher]
+
+        row += 1                    # move to the next row
+        # end for
+
+    output_sheet.cell(row, 2).value = "Generated on " + time.ctime()
+
+    return warnings
+    # end generate_teacherwise()
+
+def generate_classwise(workbook):
+    # 'sheet' contains Teacherwise timetable
+
+    # **TODO**:
+    print("Warning: This feature is under development.")
+    print("Generating Classwise from Teacherwise timetable... Done.")
+
+    # we should never write to the CLASSWISE sheet as it may corrupt our original timetable;
+    # instead we write to a CLASSWISE-GEN sheet.
+
+    CW = 'CLASSWISE-GEN'
+
+    if CW in workbook:
+        output_sheet = workbook[CW]
+    else:
+        output_sheet = workbook.create_sheet(title=CW, index=0)
+
+    input_sheet = workbook['TEACHERWISE']       # there must be TEACHERWISE sheet
+
+    # set up loops and process
+    context = {
+        'input_sheet': input_sheet,
+        'output_sheet': output_sheet,
+               # _class ( days ) subject
+        'p' : r'^(?P<_class>[\w -.]+)\s*\((?P<days>.*)\)\s*(?P<subject>\w+)$' # format "CLASS (1-3,5-6) SUBJECT"
+    }
+    process_sheet(process_teacherwise_timetable_line, context)
+
+
+    warnings = 0
+    return warnings
+
+    # end generate_classwise(filename)
+
+def process_sheet(callback, context):
+    input_sheet = context['input_sheet']
+    output_sheet = context['output_sheet']
+    p = context['p']
+
+    # end process_sheet()
+
+def process_teacherwise_timetable_line(context):
+    ...
+    # end process_teacherwise_timetable_line()
+
+if __name__ == '__main__':
+    ##########################################################
+    #
+    # process commmand line arguments
+    #
+    #
+    parser = argparse.ArgumentParser(prog='twig.py', description='Generates teacherwise (or classwise) timetable from classwise (or teacherwise) timetable.')
+    parser.version = '1.0'
+    parser.add_argument('-f', '--fullname', action='store_true', help='replace short names with full names')
+    parser.add_argument('-v', '--version', action='store_true', help='display version information')
+    parser.add_argument('-s', '--separator', action='store', help='newline separator; default is \\n')
+    parser.add_argument('-c', '--classwise', action='store_true', help='generate classwise timetable from the teacherwise timetable')
+
+    parser.add_argument('filename', type=str, action='store', nargs='?', help='file containing timetable')
+
+
+    # args = parser.parse_args(['Timetable-2023-24-Handcrafted.xlsx', 'Timetable-2023-24-Handcrafted-Teacherwise.xlsx'])
+    args = parser.parse_args()
+    # print(args)
+    if args.version:
+        print("twig.py: version 1.0.0")
+        exit(0)
+
+    expand_names = args.fullname    # True or False; default = True
+
+    if not args.filename:
+        filename = 'Timetable.xlsx'
+    else:
+        filename = args.filename
+
+
+    if not args.separator:
+        SEPARATOR = "\n"    # multi-line separator
+    else:
+        SEPARATOR = args.separator
+        if SEPARATOR == '\\n':
+            SEPARATOR = '\n'
+        print(f"Using Separator '{escape_special_chars(SEPARATOR)}' ...")
+
+    startTime = time.time()
+
+    print(f"Reading CLASSWISE timetable from '{filename}'... ", end="")
+    book = openpyxl.load_workbook(filename)
+    print("done.")
+
+    if args.classwise:
+        warnings = generate_classwise(book)
+    else:
+        warnings = generate_teacherwise(book)
+
+    # generate_free_teacher_report()
+
+    endTime = time.time()
+    # Highlight possible clashes
+    context = {'SEPARATOR': SEPARATOR}
+    teacherwise_sheet = book['TEACHERWISE']
+    total_clashes = highlight_clashes(teacherwise_sheet, context)
+    
+    print(f"Saving to TEACHERWISE sheet of '{filename}'... ", end="")
+    book.save(filename)
+    print("done.")
+    
+    print(f"Clashes: {total_clashes}")
+    print(f"Warnings: {warnings}")
+
+    print("Finished processing in %.3f seconds." % (endTime - startTime))
+    print("Have a nice day!\n")
